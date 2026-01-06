5cf48175de2dea507070a235bed805cc09eb15c2
bogilozanov <bogilozanov@gmail.com>
2026-01-06 08:48:56 +0200

    fuix party effect

--- src/components/PartyEffects.tsx
+++ src/components/PartyEffects.tsx
@@ -207,10 +207,10 @@ export default function PartyEffects({
 }
 
 const EffectsCanvas = styled.canvas`
-  position: fixed;
+  position: absolute;
   inset: 0;
   width: 100%;
   height: 100%;
   pointer-events: none;
-  z-index: 1;
+  z-index: 0;
 `;
